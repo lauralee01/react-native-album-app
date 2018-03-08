4acbdf5c0db1159d0d56e50b6143eb01881b87d3
lauralee01 <grace01>
2018-03-08 17:20:42 +0100

    add an index file

---
+++ index.js
@@ -0,0 +1,12 @@
+// Index.android.js - place code in here for android
+
+import React from 'react';
+import { Text, AppRegistry }from 'react-native';
+
+const App = () => (
+		<Text>Some Text</Text>
+		);
+
+AppRegistry.registerComponent('albums', () => App);
+
+  
