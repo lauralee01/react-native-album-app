cfa3abb9fca4c723007834363a6ac9d69fcc3663
lauralee01 <grace01>
2018-03-11 14:34:15 +0100

    add and edit AlbumList.js file

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // Index.android.js - place code in here for android
 
 import React from 'react';
-import { AppRegistry }from 'react-native';
+import { AppRegistry, View }from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-		<Header />
+	<View>
+		<Header headerText={'Albums'}/>
+		<AlbumList />
+	</View>
 		);
 
 AppRegistry.registerComponent('albums', () => App);
 
   
- 
+  
